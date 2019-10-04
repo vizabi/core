49e6513fd2b2d89a44e252182cbf9d98e9a3fa4c
Dmitry Basov <dmbasov@gmail.com>
2019-10-04 16:48:23 +0700

    use mobx as external

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
         Vizabi: './src/core/vizabi.js',
         Dataframe: './src/dataframe/dataFrame.js'
     },
+    externals: {
+        "mobx": "mobx"
+    },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
