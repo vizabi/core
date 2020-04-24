dc7f5b1a99a3c34a54a3676e3e7d92b7a30e1150
Dmitry Basov <dmbasov@gmail.com>
2020-04-24 22:04:38 +0300

    add 'extent' to size scale

--- src/core/scale/size.js
+++ src/core/scale/size.js
@@ -6,6 +6,10 @@ const defaultConfig = {
     range: [0, 20]
 }
 
+const defaults = {
+    extent: [0, 1]
+}
+
 export function size(config, parent) {
 
     applyDefaults(config, defaultConfig);
@@ -13,6 +17,12 @@ export function size(config, parent) {
 
     return assign(s, {
         ordinalScale: "point",
+        get extent() {
+            return this.config.extent || [defaults.extent[0], this.data.isConstant() ? null : defaults.extent[1]];
+        },
+        set extent(extent) {
+            this.config.extent = extent;
+        },
         get range() {
             if (this.config.range != null)
                 return this.config.range
