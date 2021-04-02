f61bc94c8e477f5c3ef854f7aeb34335b24aa4a2
Jasper <github@jasperheeffer.nl>
2021-04-03 00:23:05 +0200

    Move from webpack to rollup for dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,45 @@
-module.exports = require('./rollup.external')();
+
+/* eslint-disable no-undef */
+const path = require('path');
+
+import * as meta from "./package.json";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const copyright = `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`;
+const __DEVSERVER__ = process.env.NODE_ENV === "devserver";
+
+const output = (name, output) => ({
+    name: name,
+    dir: "dist",        
+    entryFileNames: '[name].js',
+    format: "umd",
+    banner: copyright,
+    sourcemap: true,
+    globals: {
+        "mobx": "mobx"
+    }
+});
+const external = ["mobx"];
+const plugins = () => [
+    resolve(),
+    __DEVSERVER__ && serve({
+        contentBase: ["dist"],
+        port: 4201,
+        verbose: true
+    }),
+    __DEVSERVER__ && livereload("dist/"),
+];
+
+module.exports = dir => [{
+    input: {"Vizabi": path.resolve(__dirname, 'src', 'core', 'vizabi_entry.js')},
+    output: output("Vizabi", dir), 
+    external,
+    plugins: plugins()
+}, {
+    input: {"Dataframe": path.resolve(__dirname, 'src', 'dataframe', 'dataFrame.js')},
+    output: output("Dataframe", dir),
+    external,
+    plugins: plugins()
+}]
