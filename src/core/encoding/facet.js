29c22b1b2efa0df5c172a8500d498e9d4950b0ea
angie <konzeptmeister@gmail.com>
2022-02-17 16:14:46 +0100

    Remove unneeded code in facet enc

--- src/core/encoding/facet.js
+++ src/core/encoding/facet.js
@@ -18,12 +18,6 @@ export const facet = defaultDecorator({
     base: encoding,
     defaultConfig,
     functions: {
-        get grouping () { 
-          return this.config.grouping ?? {};
-        },
-        get measures() {
-          return this.config.measures ?? [];
-        },
         //"geo"
         get row() {
             return this.config.row;
@@ -38,47 +32,7 @@ export const facet = defaultDecorator({
         },
         facet(df) {
             console.log("called transformation in facet enc", df)
-            
-            
-            const max = {};
-            [...df.values() ].forEach(v => {
-                if (!v.shapedata) return;
-                const maxForRow = d3.max(v.shapedata.split(",").map(m=>+m));
-                if (!max[v.country] || max[v.country] < maxForRow) max[v.country] = maxForRow; 
-            })
-            df.addColumn("max", (row)=>max[row.country]);
-            
             return df;
-            
-            const groupSizes = filterObject(this.grouping, 
-                (val, key) => df.key.includes(key) && val['grouping'] > 1
-            );
-            const measures = this.measures;
-  
-            if (Object.keys(groupSizes).length == 0) {
-              return df;
-            }
-  
-            let result = DataFrame([], df.key);
-            for (const row of df.rows()) {
-                // new grouped key
-                const newKeyObj = pick(row, df.key);
-                for (const dim in groupSizes) {
-                    const groupSize = groupSizes[dim]['grouping'];
-                    newKeyObj[dim] = Math.floor(+row[dim] / groupSize) * groupSize;
-                }
-                const keyStr = result.keyFn(newKeyObj);
-                // sum if group already exists, otherwise create
-                if (result.hasByStr(keyStr)) {
-                    const newRow = result.getByStr(keyStr);
-                    measures.forEach(measure => newRow[measure] += row[measure])
-                } else {
-                    const newRow = assign({}, row, newKeyObj);
-                    result.setByStr(keyStr, newRow)
-                }
-            }
-        
-            return result;
         },
         get transformationFns() {
             return {
